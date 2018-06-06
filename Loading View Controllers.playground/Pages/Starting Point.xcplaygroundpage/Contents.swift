7372753ffaf0cfb229b7fd5e280a1f88f687672b
Niels Cölle <niels@coelle-online.com>
2018-06-06 05:13:38 +0200

    Same code as at 0:58

--- Loading View Controllers.playground/Pages/Starting Point.xcplaygroundpage/Contents.swift
+++ Loading View Controllers.playground/Pages/Starting Point.xcplaygroundpage/Contents.swift
@@ -10,47 +10,30 @@ let url = NSURL(string: "http://localhost:8000/episode.json")!
 let episodeResource = Resource<Episode>(url: url as URL, parseJSON: { anyObject in
     (anyObject as? JSONDictionary).flatMap(Episode.init)
 })
-
+let sampleEpisode = Episode(id: "0", title: "initial")
 
 let sharedWebservice = Webservice()
 
 
 final class EpisodeDetailViewController: UIViewController {
-	let spinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
     let titleLabel = UILabel()
     
     convenience init(episode: Episode) {
         self.init()
         titleLabel.text = episode.title
     }
-    
-    convenience init(resource: Resource<Episode>) {
-        self.init()
-        spinner.startAnimating()
-		sharedWebservice.load(resource: resource) { [weak self] result in
-            self?.spinner.stopAnimating()
-            guard let value = result.value else { return } // TODO loading error
-            self?.titleLabel.text = value.title
-        }
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 		view.backgroundColor = .white
-        
-        spinner.hidesWhenStopped = true
-        spinner.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(spinner)
-        spinner.center(inView: view)
-        
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(titleLabel)
+		titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.constrainEdges(toMarginOf: view)
     }
 }
 
 
-let episodesVC = EpisodeDetailViewController(resource: episodeResource)
+let episodesVC = EpisodeDetailViewController(episode: sampleEpisode)
 episodesVC.view.frame = CGRect(x: 0, y: 0, width: 250, height: 300)
 
 
